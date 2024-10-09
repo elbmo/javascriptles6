d83bee93786703f9d8b00490c49d6c88c20d918e
elbmo <302090742@student.rocmondriaan.nl>
2024-10-09 10:28:50 +0200

    ee

--- js/main.js
+++ js/main.js
@@ -3,24 +3,7 @@ const age = 24;
 const hobbies = ["Voetbal, slapen, programmeren"];
 const grades = [5, 4, 6, 4];
 
-function greeting(name) {
-    let greet = "Hallo " + name + ",";
-    
-
-  console.log(greet);
-  return greet;
+function add(a, b);{
+let result = a + ;
+return result;
 }
-
-function showMessage(msg){
-    const msgBox = document.querySelector
-    const msgbox = document.querySelector(".msg-box")
-    msgbox.textContent = msg;
-}
-
-greeting(githubName);
-setTimeout(()=>{
-    greeting("tttt");
-}
-, 5000)
-showMessage("Dit is een nieuw bericht")
-
